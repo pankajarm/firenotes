1e48bea12c2e053cba60905666e45dd07fa78bdd
Pankaj Mathur <pankymathur@gmail.com>
2017-08-09 17:13:48 -0400

    REMOVING PRIVATE SECRET KEYS
    REMOVING PRIVATE SECRET KEYS

--- src/Config/config.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-  export const DB_CONFIG = {
-    apiKey: "AIzaSyBuLD8niheXvF2MIklKbaEPXA9-zU_9Hl8",
-    authDomain: "firenotes-2e685.firebaseapp.com",
-    databaseURL: "https://firenotes-2e685.firebaseio.com",
-    projectId: "firenotes-2e685",
-    storageBucket: "firenotes-2e685.appspot.com",
-    messagingSenderId: "100912880323"
-  };
